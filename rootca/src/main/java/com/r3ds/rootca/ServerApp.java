ed4109a9c1a86bca8e7fa3b6e2a61a003f0ee372
vladstojna <vlad.leic@gmail.com>
2019-12-10 12:05:10 +0000

    Root CA: signing server skeleton

---
+++ rootca/src/main/java/com/r3ds/rootca/ServerApp.java
@@ -0,0 +1,27 @@
+package com.r3ds.rootca;
+
+import java.io.IOException;
+
+/**
+ * Server app
+ */
+public class ServerApp
+{
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
+		System.out.println(ServerApp.class.getSimpleName());
+
+		if (args.length != 3) {
+			System.out.printf("USAGE: java %s port certFilePath privateKeyFilePath%n",
+				ServerApp.class.getSimpleName());
+			System.exit(0);
+		}
+
+		ServerTls server = new ServerTls(
+			Integer.parseInt(args[0]),
+			args[1],
+			args[2]);
+		server.start();
+		server.blockUntilShutdown();
+	}
+}
